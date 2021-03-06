70e81f716649ddc54076609d3ed0479536564a04
Joon An <joonseo.an@outlook.com>
2021-03-05 20:43:21 -0500

    added progress bar

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -8,6 +8,7 @@ import { Cell } from '../redux';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
+import './code-cell.css';
 
 interface CodeCellProps {
   cell: Cell;
@@ -25,11 +26,34 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   // console.log('bundle: ', bundle); == requiring typescript control
   const bundle = useTypedSelector(({ bundles }) => bundles[cell.id]);
 
-  // [IMPORTANT]
-  // props, state, context, or redux value that are used inside of the components
-  // and also used in useEffect, should be inside of []
-  // the value outside of the component should not be in []
+  // --------------------------------------------------------------------------------------------------
+  // The [issue1] below can be resolved using the double useEffect which gets bundle in the initial rendering
+  
+  // [Issue2]
+  // However, it generates the second issue.
+  // The component will cause rendering twice from useEffect.
+  // useEffect(() => {
+  //   createBundle(cell.id, cell.content);
+  // }, []); ---------------------------------------------------------------------------------
+
+  // [IMPORTANT]                                                                              |
+  // props, state, context, or redux value that are used inside of the components             |
+  // and also used in useEffect, should be inside of []                                       |
+  // the value outside of the component should not be in []                                   |
   useEffect(() => {
+    // solution                                                                <--------------|
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+    
+    // [ Issue ] about { bundle && <Preview /> }                                                       ^
+    // because of { bundle && <Preview /> },                                                           |
+    // the undefined bundle value generates leggy rendering Preview component here.                    |
+    // Specifically, after 750ms (debouncing below), bundle value is available                         |
+    // So Preview is rendering slowly than Editor.  ----------------------------------------------------
+
+
     // debouncing by implementing timer
     // whenever user types on editor, we can's send those data to the esbuildService server.
     // it will consume a lot of resource to the application and network.
@@ -43,6 +67,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   const editorOnChange = (value: string) => {
@@ -76,7 +101,23 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             editorOnChange={editorOnChange}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        <div className="progress-wrapper">
+          { 
+            !bundle || bundle.loading
+              ? (
+              
+                  <div className="progress-cover">
+                    <progress className="progress is-small is-primary" max="100">
+                      Loading
+                    </progress>
+                  </div>
+                
+              )
+              : (
+                <Preview code={bundle.code} err={bundle.err} />
+              )
+          }
+        </div>
       </div>
     </Resizable>
   );
